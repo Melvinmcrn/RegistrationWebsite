76dcbe1069f6582fb637482262555fc7f25faa5f
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-03-12 19:27:19 +0700

    redesign home page

--- src/Home.js
+++ src/Home.js
@@ -12,20 +12,33 @@ class Home extends React.Component {
         };
     }
 
+    getCard() {
+        return (
+            <div className='card'>
+                <div className='card-top'>
+                    <div className='card-topic'>Chula Open House 2020</div>
+                    <div className='card-body'>
+                        <p>Lorem ipsum dolor sit amet, turpis aliquam tellus, vel odio at sit. Urna nulla sit massa magnis. Dis diam nulla, lacus quis ligula nulla, condimentum curabitur lacus, sit leo varius nunc tortor. Scelerisque dolores, nam lorem urna nunc mattis turpis justo, eget hymenaeos mauris wisi taciti tristique, euismod est. Id et natoque molestie aliquam donec dapibus, nec aliquam hendrerit pellentesque nunc, per volutpat risus proin ultrices magna conubia. Cras litora lectus orci justo magna a, sed rhoncus mollis.</p>
+                    </div>
+                </div>
+                <div className='card-bottom'>
+                    <a href='/register' className='btn-registernow'>
+                        <a href='/register'>
+                            Register
+                        </a>
+                    </a>
+                    <div className='txt-with-link'>already register? <a href='/viewevent'>login</a></div>
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className='home-content'>
                 <div className='home-content-top'>
                     <div className='card-container'>
-                        <div className='card'>
-                            <p>Lorem ipsum dolor sit amet, turpis aliquam tellus, vel odio at sit. Urna nulla sit massa magnis. Dis diam nulla, lacus quis ligula nulla, condimentum curabitur lacus, sit leo varius nunc tortor. Scelerisque dolores, nam lorem urna nunc mattis turpis justo, eget hymenaeos mauris wisi taciti tristique, euismod est. Id et natoque molestie aliquam donec dapibus, nec aliquam hendrerit pellentesque nunc, per volutpat risus proin ultrices magna conubia. Cras litora lectus orci justo magna a, sed rhoncus mollis.</p>
-                            <a href='/register' className='btn-registernow'>
-                                <a href='/register'>
-                                    Register
-                        </a>
-                            </a>
-                            <div className='txt-with-link'>already register? <a href='/viewevent'>login</a></div>
-                        </div>
+                        {this.getCard()}
                     </div>
                 </div>
             </div>
