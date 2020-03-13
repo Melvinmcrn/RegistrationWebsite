64e4c5c79494c6dd5833316b467ff0bdcf251a67
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-03-13 14:06:17 +0700

    redesign vieweventclient and make modal get data from state and add clickable class to index

--- src/ViewEventClient.js
+++ src/ViewEventClient.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ViewEventClient.css';
 import { MdLocationOn, MdAccessTime } from 'react-icons/md';
+import { IoMdClose } from 'react-icons/io';
 
 class ViewEventClient extends React.Component {
 
@@ -8,7 +9,38 @@ class ViewEventClient extends React.Component {
         super(props);
         this.state = {
             showModal: false,
-        };
+            currentModal: null,
+            eventList: [{ eventId: 1 },
+            { eventId: 2 },
+            { eventId: 3 },
+            { eventId: 4 },
+            { eventId: 5 },
+            { eventId: 6 },
+            { eventId: 7 },
+            { eventId: 8 },
+            { eventId: 9 },
+            { eventId: 10 },
+            { eventId: 11 },
+            { eventId: 12 },
+            { eventId: 13 },
+            { eventId: 14 }],
+            eventData: {
+                eventId_1: { name: 'Intaniverse', loc: 'Faculty of Engineering', time: '20 June 2019, 8:30PM', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_2: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_3: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_4: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_5: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_6: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_7: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_8: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_9: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_10: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_11: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_12: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_13: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+                eventId_14: { name: 'Event_2', loc: 'loc_2', time: 'time_2', detail: 'klsdmvklsdmvlsdkmvlskdvmlskmvdsldkvmsldkvm' },
+            }
+        }
 
         this.showHideModal = this.showHideModal.bind(this);
     }
@@ -25,97 +57,68 @@ class ViewEventClient extends React.Component {
 
                 <div className='event-container'>
                     {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
-                    {this.getEventCard()}
                 </div>
-                {/* Engineering Event */}
-                {/* {generateEventContainer('Chula Engineering', 'engineer-event', 'Building 3', '21-22 March 2020', 'Open', 'Register')} */}
-
-                {/* CBS Event */}
-                {/* {generateEventContainer('CBS', 'cbs-event', 'Mahit Building', '21-22 March 2020', 'Pending', 'Cancel')} */}
-
-                {/* MDCU Event */}
-                {/* {generateEventContainer('MDCU', 'mdcu-event', 'CP Building', '21-22 March 2020', 'Registered', 'Cancel')} */}
 
             </div>
         );
     }
 
     getEventCard() {
-        return (
-            <div className='event-card' onClick={this.showHideModal}>
-                <div className='event-name'>
-                    Intaniverse
-                </div>
-                <div className='event-detail'>
-                    <h2><MdLocationOn className='icon' />Faculty of Engineering</h2>
-                    <h2><MdAccessTime className='icon' />20 June 2019, 8.30PM</h2>
-                </div>
+        return this.state.eventList.map((item) => {
+            let eventData = this.state.eventData['eventId_' + item.eventId];
+            return (
+                <div key={'eventId_' + item.eventId} name='hell' className='event-card clickable' onClick={() => this.showHideModal('eventId_' + item.eventId)}>
+                    <div className='event-name'>
+                        {eventData.name}
+                    </div>
+                    <div className='event-detail'>
+                        <h2><MdLocationOn className='icon' />{eventData.loc}</h2>
+                        <h2><MdAccessTime className='icon' />{eventData.time}</h2>
+                    </div>
 
-            </div>
-        );
+                </div>
+            );
+        });
     }
 
     getEventModal() {
-        return (
-            <div className={'modal-container ' + (this.state.showModal ? 'show-modal' : '')}  onClick={this.showHideModal}>
+        let eventId = this.state.currentModal;
+        let eventData = this.state.eventData[eventId];
+        return !eventData ? '' : (
+            <div className={'modal-container ' + (this.state.showModal ? 'show-modal' : '')}>
                 <div className='event-modal'>
-                    HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
-                HELLO<br />
+                    <div className='modal-header'>
+                        <div className='left'>
+                            <div className='event-name'>
+                                {eventData['name']}
+                            </div>
+                        </div>
+                        <div className='right'>
+                            <IoMdClose className='icon clickable' onClick={() => this.showHideModal(null)} />
+                        </div>
+                    </div>
+                    <div className='event-detail'>
+                        <h2><MdLocationOn className='icon' />{eventData['loc']}</h2>
+                        <h2><MdAccessTime className='icon' />{eventData['time']}</h2>
+                        <h2>{eventData['detail']}</h2>
+                    </div>
                 </div>
             </div>
         );
     }
 
-    showHideModal() {
+    showHideModal(eventId) {
+        
+        if(!this.state.showModal && eventId) {
+            this.setState({
+                currentModal: eventId,
+            });
+        }
+
         this.setState({
             showModal: !this.state.showModal,
-        })
+        });
     }
 }
-
-function generateEventContainer(name, classname, loc, time, eventStatus, userStatus) {
-    return (
-        <div className={'event-container ' + classname}>
-            <div className='left'>
-                <h1 className='event-name'>{name}</h1>
-                <div className='event-detail'>
-                    <h2><MdLocationOn className='icon' />{loc}</h2>
-                    <h2><MdAccessTime className='icon' />{time}</h2>
-                </div>
-            </div>
-
-            <div className='right'>
-                <h2 className={'top-status ' + eventStatus}>{eventStatus}</h2>
-                <div className={'bottom-btn ' + userStatus}>{userStatus}</div>
-            </div>
-
-        </div>
-    )
-}
-
 
 export default ViewEventClient;
